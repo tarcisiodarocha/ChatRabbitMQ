22991484f3a99aea20d414e3110859cfb631711e
Ubuntu <ubuntu@ip-172-31-2-150.ec2.internal>
2025-11-02 21:11:24 +0000

    Finalizando a etapa 1 do projeto

--- src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
+++ src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
@@ -8,14 +8,16 @@ public class Chat {
 
   public static void main(String[] argv) throws Exception {
     ConnectionFactory factory = new ConnectionFactory();
-    factory.setHost("ip-da-instancia-da-aws"); // Alterar
-    factory.setUsername("usuário-do-rabbitmq-server"); // Alterar
-    factory.setPassword("senha-do-rabbitmq-server"); // Alterar
+    
+    factory.setHost("ip-da-instancia-da-aws");          // Alterar
+    factory.setUsername("usuário-do-rabbitmq-server");  // Alterar nome
+    factory.setPassword("senha-do-rabbitmq-server");    // Alterar
     factory.setVirtualHost("/");
     Connection connection = factory.newConnection();
     Channel channel = connection.createChannel();
     
     String QUEUE_NAME = "minha-fila";
+    
                       //(queue-name, durable, exclusive, auto-delete, params); 
     channel.queueDeclare(QUEUE_NAME, false,   false,     false,       null);
     
