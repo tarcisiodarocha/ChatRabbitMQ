aa550b77dfaee4823fde05443bd1f14808e14c84
Ubuntu <ubuntu@ip-172-31-26-14.ec2.internal>
2026-04-13 23:30:05 +0000

    feat: Inicia implementação da etapa 1

--- src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
+++ src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
@@ -1,34 +1,74 @@
 package br.ufs.dcomp.ChatRabbitMQ;
 
-import com.rabbitmq.client.*;
-
 import java.io.IOException;
+import java.util.Scanner;
+
+import com.rabbitmq.client.*;
 
 public class Chat {
 
   public static void main(String[] argv) throws Exception {
+
     ConnectionFactory factory = new ConnectionFactory();
-    factory.setHost("ip-da-instancia-da-aws"); // Alterar
-    factory.setUsername("usuário-do-rabbitmq-server"); // Alterar
-    factory.setPassword("senha-do-rabbitmq-server"); // Alterar
+    factory.setHost("18.206.187.39");
+    factory.setUsername("admin");
+    factory.setPassword("password");
     factory.setVirtualHost("/");
+
     Connection connection = factory.newConnection();
     Channel channel = connection.createChannel();
-    
-    String QUEUE_NAME = "minha-fila";
-                      //(queue-name, durable, exclusive, auto-delete, params); 
-    channel.queueDeclare(QUEUE_NAME, false,   false,     false,       null);
-    
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("User: ");
+    String usuario = scanner.nextLine();
+
+    String QUEUE_NAME = usuario;
+
+    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
     Consumer consumer = new DefaultConsumer(channel) {
-      public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)           throws IOException {
+      public void handleDelivery(String consumerTag, Envelope envelope,
+          AMQP.BasicProperties properties, byte[] body) throws IOException {
 
         String message = new String(body, "UTF-8");
-        System.out.println(message);
-
+        System.out.println("\n" + message);
+        System.out.print("<< ");
       }
     };
-                      //(queue-name, autoAck, consumer);    
-    channel.basicConsume(QUEUE_NAME, true,    consumer);
-    
+
+    channel.basicConsume(QUEUE_NAME, true, consumer);
+
+    String destinatario = "";
+
+    System.out.print("<< ");
+
+    while (true) {
+
+      String input = scanner.nextLine();
+
+      if (input.startsWith("@")) {
+        destinatario = input.substring(1);
+        System.out.print("@" + destinatario + "<< ");
+        continue;
+      }
+
+      if (destinatario.isEmpty()) {
+        System.out.println("Escolha um destinatário com @usuario");
+        System.out.print("<< ");
+        continue;
+      }
+
+      String mensagem = "@" + usuario + ": " + input;
+
+      channel.basicPublish(
+          "",
+          destinatario,
+          null,
+          mensagem.getBytes("UTF-8")
+      );
+
+      System.out.printl("@" + destinatario + "<< ");
+    }
   }
 }
